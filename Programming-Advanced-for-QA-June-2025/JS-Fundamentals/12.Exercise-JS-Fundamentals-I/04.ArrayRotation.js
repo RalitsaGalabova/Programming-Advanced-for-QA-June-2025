73803733f17e475f1df45b4f70a62bea0c2c6707
RalitsaGalabova <ralica_gulubova@yahoo.com>
2025-08-11 15:35:48 +0300

    Delete Programming-Advanced-for-QA-June-2025 directory

--- Programming-Advanced-for-QA-June-2025/JS-Fundamentals/12.Exercise-JS-Fundamentals-I/04.ArrayRotation.js
+++
@@ -1,8 +0,0 @@
-function arrayRotation(arr, countRotations){
-    for(let rotations = 1; rotations<=countRotations; rotations++){
-        let firstElement = arr.shift();
-        arr.push(firstElement);
-    }
-
-    console.log(arr.join(" "));
-}
